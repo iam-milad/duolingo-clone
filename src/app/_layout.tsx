147f8895316262690ffe229266bcaad6def7eb91
iam-milad <miladnouri073@hotmail.com>
2026-07-02 21:30:36 +0100

    implement app business logic and language selection ui

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout() {
         <Stack.Screen name="index" />
         <Stack.Screen name="onboarding" />
         <Stack.Screen name="(auth)" />
+        <Stack.Screen name="language-selection" />
       </Stack>
     </ClerkProvider>
   );
